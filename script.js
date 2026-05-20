448607de43f1bcc8588a24ec8ec7663045b6da45
Seiti Katsumi <SeitiKatsumi@users.noreply.github.com>
2026-05-20 14:28:01 -0300

    Add fading hero image slideshow

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const nav = document.querySelector(".nav");
 const leadForm = document.querySelector("#leadForm");
 const formStatus = document.querySelector("#formStatus");
 const appUrl = "https://flebogram.elevenmind.com.br/";
+const heroSlides = Array.from(document.querySelectorAll(".hero-slide"));
 
 menuButton?.addEventListener("click", () => {
   const isOpen = nav.classList.toggle("is-open");
@@ -25,3 +26,18 @@ leadForm?.addEventListener("submit", (event) => {
     window.location.href = appUrl;
   }, 700);
 });
+
+if (heroSlides.length > 1) {
+  let activeHeroSlide = heroSlides.findIndex((slide) => slide.classList.contains("is-active"));
+
+  if (activeHeroSlide < 0) {
+    activeHeroSlide = 0;
+    heroSlides[0].classList.add("is-active");
+  }
+
+  window.setInterval(() => {
+    heroSlides[activeHeroSlide].classList.remove("is-active");
+    activeHeroSlide = (activeHeroSlide + 1) % heroSlides.length;
+    heroSlides[activeHeroSlide].classList.add("is-active");
+  }, 6500);
+}
